bedf0856895302f2824c2e63d792f2e6f4016679
Ferza17 <fery.aditya@students.amikom.ac.id>
2019-10-03 12:20:49 +0700

    commit kedua

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+var Tesseract = window.Tesseract;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <header className="header">
+          <h1>Opticar Charecter Recognition </h1>
+        </header>
+
+        { /* File uploader */ }
+        <section className="hero">
+          <label className="fileUploaderContainer">
+            Klik untuk upload File
+            <input type="file" id="fileUploader" multiple />
+          </label>
+
+          <div>
+            { /* Previews will be shown here */ }
+          </div>
+
+          <button className="button">Generate</button>
+        </section>
+
+        { /* Results */ }
+        <section className="results">
+
+          <div className="results__result">
+            <div className="results__result__image">
+              <img width="250px" />
+            </div>
+            <div className="results__result__info">
+              <div className="results__result__info__codes">
+                <small>{ /* Confidence score */ }</small>
+              </div>
+              <div className="results__result__info__codes">
+                <small>{ /* Pattern output */ }</small>
+              </div>
+              <div className="results__result__info__text">
+                <small>{ /* Full output */ }</small>
+              </div>
+            </div>
+            <hr />
+          </div>
+
+          <div className="results__result">
+            { /* Additional output if more than one document is processed */ }
+          </div>
+
+        </section>
       </div>
     );
   }
